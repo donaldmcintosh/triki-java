460b685273082fc88908b4eae0303e54754420b8
Donald McIntosh <dmcintosh@scottishpower.com>
2019-06-17 15:19:20 +0100

    TRK-30: Interim fix

--- triki-core/src/main/groovy/net/opentechnology/triki/auth/WicketApplication.java
+++ triki-core/src/main/groovy/net/opentechnology/triki/auth/WicketApplication.java
@@ -1,14 +1,18 @@
 package net.opentechnology.triki.auth;
 
-import net.opentechnology.triki.auth.pages.HelloWorld;
 import net.opentechnology.triki.auth.pages.LoginPage;
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class WicketApplication extends WebApplication {
 
   @Override
   public Class<? extends Page> getHomePage() {
+    getComponentInstantiationListeners().add(new SpringComponentInjector(this,
+        WebApplicationContextUtils.getRequiredWebApplicationContext(
+            getServletContext())));
     return LoginPage.class;
   }
 
--- triki-core/src/main/groovy/net/opentechnology/triki/auth/pages/LoginPage.java
+++ triki-core/src/main/groovy/net/opentechnology/triki/auth/pages/LoginPage.java
@@ -1,6 +1,13 @@
 package net.opentechnology.triki.auth.pages;
 
+import java.util.Optional;
+import javax.inject.Inject;
+import net.opentechnology.triki.auth.AuthenticationException;
+import net.opentechnology.triki.auth.AuthenticationManager;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
@@ -9,6 +16,10 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
 
 public class LoginPage extends ParentPage {
+
+  @Inject
+  private AuthenticationManager authMgr;
+
   public LoginPage(final PageParameters parameters) {
     super(parameters);
     add(new LoginForm("loginForm"));
@@ -19,20 +30,41 @@ public class LoginPage extends ParentPage {
     private String password;
     private String loginStatus;
 
+    WebMarkupContainer loginFeedback = new WebMarkupContainer("loginFeedback"){
+      @Override
+      public final boolean isVisible(){
+        return loginStatus != null;
+      }
+    };
+
     public LoginForm(String id) {
       super(id);
       setDefaultModel(new CompoundPropertyModel(this));
 
-      add(new TextField("username"));
+      FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
+      add(feedbackPanel);
+      TextField usernameTextfield = new TextField("username");
+      usernameTextfield.setRequired(true);
+      add(usernameTextfield);
+
       add(new PasswordTextField("password"));
-      add(new Label("loginStatus"));
+      loginFeedback.add(new Label("loginStatus"));
+      add(loginFeedback);
     }
 
+    @Override
     public final void onSubmit() {
-      if(username.equals("test") && password.equals("test"))
-        loginStatus = "Congratulations!";
-      else
+      try {
+        Optional<Resource> signedInPerson = authMgr.authenticate(username, password);
+        if(signedInPerson.isPresent()){
+          loginStatus = "Congratulations!!";
+        }
+        else {
+          loginStatus = "Wrong username or password !";
+        }
+      } catch (AuthenticationException e) {
         loginStatus = "Wrong username or password !";
+      }
     }
   }
 }
