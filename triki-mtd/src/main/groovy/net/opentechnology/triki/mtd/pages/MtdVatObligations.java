2c410f386cdd947146effd7ea78bd9f3de8ffc62
Donald McIntosh <dmcintosh@scottishpower.com>
2019-07-11 11:54:46 +0100

    TRK-31: Working on validation

--- triki-mtd/src/main/groovy/net/opentechnology/triki/mtd/pages/MtdVatObligations.java
+++ triki-mtd/src/main/groovy/net/opentechnology/triki/mtd/pages/MtdVatObligations.java
@@ -5,6 +5,7 @@ import java.util.List;
 import net.opentechnology.triki.auth.components.FeedbackListContainer;
 import net.opentechnology.triki.auth.components.FeedbackStringContainer;
 import net.opentechnology.triki.auth.resources.SessionUtils;
+import net.opentechnology.triki.mtd.validators.FormFieldValidator;
 import org.apache.wicket.Component;
 import org.apache.wicket.feedback.ComponentFeedbackMessageFilter;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -30,7 +31,7 @@ public class MtdVatObligations extends MtdVatManage {
     super.onConfigure();
   }
 
-  private class MtdVatObligationsForm extends Form {
+  private class MtdVatObligationsForm extends Form<MtdVatObligationsForm> {
 
     private SessionUtils sessionUtils;
     private String vrn;
@@ -46,21 +47,17 @@ public class MtdVatObligations extends MtdVatManage {
       setDefaultModel(new CompoundPropertyModel(this));
 
       TextField vrn = new TextField("vrn");
+      FormFieldValidator formFieldValidator = new FormFieldValidator("VRN");
+      vrn.add(formFieldValidator);
       add(vrn);
-
       FeedbackListContainer vrnFeedback = new FeedbackListContainer("vrnFeedback");
-      vrnFeedback.setFilter(new ComponentFeedbackMessageFilter(vrnFeedback));
+      vrnFeedback.setFilter(new ComponentFeedbackMessageFilter(vrn));
       add(vrnFeedback);
 
       List<String> dateRange = Arrays.asList("12 months", "2 years");
       add(new DropDownChoice<String>("dateRange", new Model(), dateRange));
       List<String> status = Arrays.asList("Fulfilled", "Open");
       add(new DropDownChoice<String>("status", new Model(), status));
-
-//      HiddenField dateRange = new HiddenField("dateRange");
-//      add(dateRange);
-//      HiddenField status = new HiddenField("status");
-//      add(status);
 
       HiddenField hmrcHeaders = new HiddenField("hmrcHeaders");
       add(hmrcHeaders);
