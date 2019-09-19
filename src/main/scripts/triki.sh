b6f285ca55815fae3a87ef08a55d688525c71a06
Donald McIntosh <donaldbmcintosh@yahoo.co.uk>
2019-09-19 13:05:40 +0100

    fixed startup

--- src/main/scripts/triki.sh
+++ src/main/scripts/triki.sh
@@ -25,15 +25,16 @@ then
    DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000"
 fi
 
+# Colon separated paths
 if [ ! -z ${TRIKI_MODULES} ]
 then
-  for jar in `ls -1 $TRIKI_MODULES`; do
-    TRIKI_MODULES=$TRIKI_MODULES/$jar
+  for jar in `echo $TRIKI_MODULES | sed "s/:/ /g"`; do
+    MODULES_CLASSPATH=$MODULES_CLASSPATH:`ls -1 $LOCAL_DEPLOY/modules/*$jar*`
   done
-  echo Adding triki modules $TRIKI_MODULES
+  echo Adding triki modules $MODULES_CLASSPATH
 fi
 
 TRIKI_JAR=`ls -1 lib/triki*.jar`
-export CLASSPATH=$CLASSPATH:$TRIKI_JAR:$TRIKI_MODULES
+export CLASSPATH=$CLASSPATH:$TRIKI_JAR:$MODULES_CLASSPATH
 
 java $DEBUG -Dcontent_dir=$1 -Dport=$2 net.opentechnology.triki.core.boot.TrikiMain
