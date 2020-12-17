644910d0492bbad747f1b8d57effa7fcf2e64192
Donald McIntosh <dmcintosh@opentechnology.net>
2020-12-17 14:20:07 +0000

    added declarative pipeline

---
+++ PipelineDec.groovy
@@ -0,0 +1,31 @@
+pipeline{
+    agent any
+    stages {
+        stage('Clone sources') {
+	    steps {
+                git credentialsId: 'jenkins-robin', url: 'git@github.com:donaldmcintosh/triki.git'
+	    }
+        }
+        stage('Gradle build') {
+	    steps {
+                sh "./gradlew clean build publishToMavenLocal"
+	        cucumber fileIncludePattern: 'build/dev.json'
+	    }
+        }
+        stage('Deploy local') {
+	    steps{
+                sh "./gradlew unzipDistrib"
+	    }
+        }
+        stage('Release'){
+	    steps{
+                timeout(time: 10, unit: 'MINUTES') {
+                    input message: "Release?", ok: "Yes"
+                }
+	        sshagent (credentials: ['jenkins-robin']) {
+                   sh "git clean -fd; ./gradlew release -Prelease.useAutomaticVersion=true"
+                }
+	    }
+        }
+    }
+}
